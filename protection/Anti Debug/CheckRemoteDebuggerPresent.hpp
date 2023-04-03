3aca298df2cc2117b972cbcec2f689a0a927cd9b
Kai <kaio@aidswa.re>
2023-04-04 00:32:02 +0100

    gitignore

--- protection/Anti Debug/CheckRemoteDebuggerPresent.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Windows.h>
-
-BOOL CheckRemoteDebuggerPresentAPI( VOID )
-{
-	BOOL m_bIsDebugging = FALSE;
-	CheckRemoteDebuggerPresent( GetCurrentProcess( ), &m_bIsDebugging );
-	return m_bIsDebugging;
-}
